96f374cdcc73e4d4cf92fde0b4db2c8a213ac6df
awawr <128203465+awawr@users.noreply.github.com>
2025-04-04 09:05:38 +0800

    Add files via upload

--- account-creation/account_generator.py
+++ account-creation/account_generator.py
@@ -1,9 +1,9 @@
 import time
 import random
-from pathlib import Path
+import string
 from bs4 import BeautifulSoup
 from patchright.sync_api import sync_playwright
-from pymailtm import MailTm
+from pymailtm import MailTm, Account
 
 brainrot = ["skibidi", "toilet", "rizz", "brainrot", "gyatt", "ohio", "tax", "gooner", "comp"]
 names = ["zentrr", "arctic", "rodrigo", "gonzalez"]
@@ -11,15 +11,38 @@ triggers = ["pedo", "minor", "racist", "hard r"]
 char = ["o", "j", "0", "x", "1", "l", "9"]
 char2 = ["y", "F", "0", "a", "g", "9", "6"]
 
-def main():
-    client = MailTm()
-    account = client.get_account() # Create temp email
+def generate_userpass():
+    username = random.choice(brainrot) + random.choice(names) + random.choice(triggers)
+    password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    return (username, password)
+
+def verify_email(email):
+    email_html = None
+    messages = None
+    while True: 
+        messages = email.get_messages()
+        if (len(messages) >= 2): # Wait for 2 or more messages to be received
+            break
+        time.sleep(3)
+    for i in range(len(messages)):
+        if "please confirm your email address" in messages[i].subject: # Find confirmation email
+            email_html = str(messages[i].html)
+            break
+    soup = BeautifulSoup(email_html, features="html.parser") # Parse email HTML
+    verify_link = None
+    links = soup.select('a[style="background-color:#f12c18; border:0px solid #f12c18; border-color:#f12c18; border-radius:50px; border-width:0px; color:#ffffff; display:inline-block; font-size:16px; font-weight:bold; letter-spacing:0px; line-height:normal; padding:10px 20px 10px 20px; text-align:center; text-decoration:none; border-style:solid; font-family:helvetica,sans-serif;"]')
+    for link in links:
+        verify_link = link.get("href")
+    return verify_link
+
+def generate_account(email: Account, name: str, password: str, talents: list[str], genres: list[str], artists: list[str], username: str = None, profile_pic: str = None):
     with sync_playwright() as p:
         with p.chromium.launch_persistent_context(
             user_data_dir="browser", 
             channel="chrome", 
             headless=False, 
-            no_viewport=True, 
+            no_viewport=True,
+            args = [f"--disable-extensions-except=PLUGIN PATH HERE", f"--load-extension=PLUGIN PATH HERE"]
             ) as browser:
 
             browser.clear_cookies() # Clear old cookies
@@ -27,51 +50,59 @@ def main():
             page.set_default_navigation_timeout(0)
             page.set_default_timeout(0)
             page.goto('https://www.bandlab.com/sign-up')
-            page.get_by_placeholder("Enter your name").fill(random.choice(brainrot) + random.choice(names) + random.choice(triggers)) # Name
-            page.get_by_placeholder("you@example.com").fill(account.address) # Email
-            page.get_by_placeholder("Enter at least 6 characters").fill() # Password
+            page.get_by_placeholder("Enter your name").fill(name) # Name
+            page.get_by_placeholder("you@example.com").fill(email.address) # Email
+            page.get_by_placeholder("Enter at least 6 characters").fill(password) # Password
             page.locator("select[ng-model='pickDate.day']").select_option("1") # Day
             page.locator("select[ng-model='pickDate.month']").select_option("1") # Month
             page.locator("select[ng-model='pickDate.year']").select_option("2000") # Year
             page.get_by_role("button", name="Sign up").click()
 
-            # Todo: captcha solve
-
             page.wait_for_url("https://www.bandlab.com/onboarding")
-            # page.locator("div[class='quick-upload-cover-cta form-field-upload-picture-img-circle']").click()
-            # page.locator("input[type='file']").set_input_files("000c269a70f1e0f246ea44d947027167.jpg")
-            # page.get_by_role("button").get_by_text("Upload Picture").click() # Upload profile picture
-            # page.get_by_placeholder("Set a custom username").fill() # Username
+            if profile_pic != None:
+                page.locator("div[class='quick-upload-cover-cta form-field-upload-picture-img-circle']").click()
+                page.locator("input[type='file']").set_input_files(profile_pic)
+                page.get_by_role("button").get_by_text("Upload Picture").click() # Upload profile picture
+            if (username != None):
+                page.get_by_placeholder("Set a custom username").fill(username) # Username
             page.get_by_role("button", name="Continue").click()
             page.wait_for_selector("div[class='complete-profile-goals']")
             page.get_by_role("button", name="Continue").click()
-            page.get_by_label("Tell Us More About You").get_by_text("Other").click() # Talents
+
+            # Talents
+            t_element = page.get_by_label("Tell Us More About You")
+            for t in talents:
+                try:
+                    t_element.get_by_text(t).click()
+                except:
+                    print(f"No talent found for: {t}")
             page.get_by_role("button", name="Continue").click()
-            page.get_by_label("Pick Your Favorite Genres").get_by_text("Other").click() # Favourite genres
+
+            # Favourite genres
+            g_element = page.get_by_label("Pick Your Favorite Genres")
+            for g in genres:
+                try:
+                    g_element.get_by_text(g).click()
+                except:
+                    print(f"No genre found for: {t}")
             page.get_by_role("button", name="Continue").click()
-            page.get_by_placeholder("Search Artists…").fill("Deez Nuts")
-            page.locator("div[class='mentions-suggestions-fullname']").filter(has_text="Deez Nuts").nth(1).click() # Click favourite artist
+
+            # Search and select inspired-by artists
+            for a in artists:
+                page.get_by_placeholder("Search Artists…").fill(a)
+                page.locator("div[class='mentions-suggestions-fullname']").filter(has_text=a).nth(1).click()
             page.get_by_role("button", name="Done").click()
 
             page.wait_for_url("https://www.bandlab.com/feed/trending") # Wait for sign up to finish
 
-            # Terrible implementation of email verification
-            email_html = None
-            while True:
-                messages = account.get_messages()
-                if (len(messages) > 1):
-                    email_html = str(messages[1].html)
-                    break
-                time.sleep(3)
-            soup = BeautifulSoup(email_html, features="html.parser")
-            verify_link = None
-            links = soup.select('a[style="background-color:#f12c18; border:0px solid #f12c18; border-color:#f12c18; border-radius:50px; border-width:0px; color:#ffffff; display:inline-block; font-size:16px; font-weight:bold; letter-spacing:0px; line-height:normal; padding:10px 20px 10px 20px; text-align:center; text-decoration:none; border-style:solid; font-family:helvetica,sans-serif;"]')
-            for link in links:
-                verify_link = link.get("href")
-            print(verify_link)
-            page.goto(verify_link)
-            # Todo: Email verification requires captcha solve
-            input()
+            page.goto(verify_email(email))
 
+def main():
+    client = MailTm()
+    account = client.get_account() # Create temp email
+    userpass = generate_userpass()
+    generate_account(account, userpass[0], userpass[1], ["Other"], ["Other"], ["Deez Nuts"])
+    input()
+    
 if __name__ == "__main__":
     main()
